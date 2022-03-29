88cd64da229ba6c030534429a687dcdac10f4d40
asmae7 <102560333+asmae7@users.noreply.github.com>
2022-03-29 14:01:25 +0000

    Create Footer.vue

---
+++ Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>Asmae SAKOURI &copy; 2022</p>
+  </footer>
+</template>
+
+<style scoped>
+a {
+  color: #333;
+}
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
