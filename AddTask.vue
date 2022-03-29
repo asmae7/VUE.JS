d78c0cccce6b31fc24b3d3224bbcd26fd1a07a14
asmae7 <102560333+asmae7@users.noreply.github.com>
2022-03-29 14:00:38 +0000

    Create AddTask.vue

---
+++ AddTask.vue
@@ -0,0 +1,86 @@
+<template>
+  <form @submit="onSubmit" class="add-form">
+    <div class="form-control">
+      <label>Tâche</label>
+      <input type="text" v-model="text" name="text" placeholder="Ajouter un rappel" />
+    </div>
+    <div class="form-control">
+      <label>Jour et heure</label>
+      <input
+        type="text"
+        v-model="day"
+        name="jour"
+        placeholder="ajouter jour et heure"
+      />
+    </div>
+    <div class="form-control form-control-check">
+      <label>Définir un rappel</label>
+      <input type="checkbox" v-model="reminder" name="rappel" />
+    </div>
+
+    <input type="submit" value="Sauvegarde" class="btn btn-block" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'AddTask',
+  data() {
+    return {
+      text: '',
+      day: '',
+      reminder: false,
+    }
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault()
+      if (!this.text) {
+        alert("Attention! il faut ajouter une tâche")
+        return
+      }
+      const newTask = {
+       id: Math.floor(Math.random() * 100000),
+        text: this.text,
+        day: this.day,  
+        reminder: this.reminder,
+      }
+      this.$emit('add-task', newTask)
+      this.text = ''
+      this.day = ''
+      this.reminder = false
+    },
+  },
+}
+</script>
+
+<style scoped>
+.add-form {
+  margin-bottom: 40px;
+}
+.form-control {
+  margin: 20px 0;
+}
+.form-control label {
+  display: block;
+}
+.form-control input {
+  width: 100%;
+  height: 40px;
+  margin: 5px;
+  padding: 3px 7px;
+  font-size: 17px;
+}
+.form-control-check {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.form-control-check label {
+  flex: 1;
+}
+.form-control-check input {
+  flex: 2;
+  height: 20px;
+}
+</style>
