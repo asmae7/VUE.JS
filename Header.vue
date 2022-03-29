3376832e5cd64d894ce1a18613b4acbfcbc0da16
asmae7 <102560333+asmae7@users.noreply.github.com>
2022-03-29 13:42:27 +0000

    Create Header.vue

---
+++ Header.vue
@@ -0,0 +1,33 @@
+<template>
+   <header>
+       <h1>
+         {{title}}
+       </h1>
+       <Button @toggle-add-task="$emit('toggle-add-task')" :text=" showAddTask ? 'Fermer'  : 'Ajouter une tâche'" color="black"/>
+       
+       </header> 
+</template>
+<script>
+import Button from "./Button"
+
+export default {
+    name:"header",
+    props:{
+        title:String,
+        showAddTask:Boolean
+    },
+    components:{
+        Button
+    }
+}
+ 
+
+</script>
+<style scoped>
+  Header{
+      display:flex ;
+       justify-content: space-between;
+       align-items: center;
+       margin-bottom: 20px;
+  }
+</style>
